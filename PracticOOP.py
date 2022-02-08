5b24d99a85316416a1e5c82d2b20cd76b52ba8bb
Sogrinn <Alexsandr99.99.99@gmail.com>
2022-02-08 14:01:55 +0300

    ex 3

--- PracticOOP.py
+++ PracticOOP.py
@@ -38,7 +38,7 @@ print(task_1.exponentiation())
 print(task_1.num_1)"""
 
 
-class Calculator:
+'''class Calculator:
 
     def calc(self):
         num_1 = input('Num_1 :')
@@ -55,3 +55,43 @@ class Calculator:
 
 calc = Calculator()
 print(calc.calc())
+'''
+
+
+class Task:
+    vowels = 'уеыаоэяию'
+
+    def __init__(self, user_input=input('!:'), long_of_input=None):
+        self.user_input = user_input
+        self.long_of_input = long_of_input
+
+    def long(self):
+        self.long_of_input = len(self.user_input)
+
+    def fun(self):
+        if self.user_input.isdigit:
+            sum_num = 0
+            for num in self.user_input[::2]:
+                sum_num += int(num)
+                return sum_num * self.long_of_input
+        else:
+            count_consonants = 0
+            count_vowels = 0
+            for_print_vowels = ''
+            for_print_consonants = ''
+            for l in self.user_input:
+                if l in self.vowels:
+                    count_vowels += 1
+                    for_print_vowels += l
+                else:
+                    count_consonants += 1
+                    for_print_consonants += l
+            if count_vowels * count_consonants <= self.long_of_input:
+                return for_print_vowels
+            else:
+                return for_print_consonants
+
+
+task = Task()
+task.long()
+print(task.fun())
